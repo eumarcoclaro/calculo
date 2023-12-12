382d3c3c077c523cbcf5da28c082be58b6d3d2ab
eumarcoclaro <153576425+eumarcoclaro@users.noreply.github.com>
2023-12-12 00:45:31 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,25 @@
-function process_form() {
-    $custoDireto = $_POST['custoDireto'];
-    $horasTrabalho = $_POST['horasTrabalho'];
-    $custoHora = $_POST['custoHora'];
-    $margemLucro = $_POST['margemLucro'];
-    $taxaImposto = $_POST['taxaImposto'];
+<?php
+// Verifica se os dados foram enviados pelo método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
+    $custoDireto = isset($_POST["custoDireto"]) ? floatval($_POST["custoDireto"]) : 0;
+    $horasTrabalho = isset($_POST["horasTrabalho"]) ? floatval($_POST["horasTrabalho"]) : 0;
+    $custoHora = isset($_POST["custoHora"]) ? floatval($_POST["custoHora"]) : 0;
+    $margemLucro = isset($_POST["margemLucro"]) ? floatval($_POST["margemLucro"]) : 0;
+    $taxaImposto = isset($_POST["taxaImposto"]) ? floatval($_POST["taxaImposto"]) : 0;
 
-    // Cálculo do custo total
+    // Calcula o custo total
     $custoTotal = $custoDireto + ($horasTrabalho * $custoHora);
 
-    // Cálculo do preço de venda
+    // Calcula o preço de venda sem impostos
     $precoVenda = $custoTotal * (1 + ($margemLucro / 100));
 
-    // Ajuste para impostos
-    $precoFinal = $precoVenda / (1 - ($taxaImposto / 100));
+    // Ajusta para impostos
+    $precoFinal = $precoVenda * (1 + ($taxaImposto / 100));
 
-    // Redirecionar para uma página com os resultados
-    wp_redirect(home_url('/resultado-orcamento?preco=' . $precoFinal));
-    exit;
+    // Exibe o resultado
+    echo "Custo Total: R$ " . number_format($custoTotal, 2) . "<br>";
+    echo "Preço de Venda (sem impostos): R$ " . number_format($precoVenda, 2) . "<br>";
+    echo "Preço Final (com impostos): R$ " . number_format($precoFinal, 2);
 }
-add_action('admin_post_process_form', 'process_form');
-add_action('admin_post_nopriv_process_form', 'process_form');
+?>
